72400c6fa2eb2189888cc08540735a2706d499fb
Pedja Josifovic <pjosifovic@yahoo.com>
2024-03-09 17:00:10 -0800

    Create index.js M09

---
+++ M09/index.js
@@ -0,0 +1,22 @@
+var mybutton = document.querySelector("button");
+mybutton.addEventListener("click", function (event) {
+  var element = document.getElementsByTagName("div");
+  for (index = element.length - 1; index >= 0; index--) {
+    element[index].parentNode.removeChild(element[index]);
+  }
+  var canvas = document.createElement("div");
+  canvas.setAttribute("id", "canvas");
+  document.body.appendChild(canvas);
+
+  // Let us stop the propagation of events
+  event.stopPropagation();
+});
+
+addEventListener("click", function (event) {
+  var dot = document.createElement("div");
+  dot.className = "dot";
+  dot.style.left = event.pageX - 4 + "px";
+  dot.style.top = event.pageY - 4 + "px";
+  var canvas = document.querySelector("#canvas");
+  canvas.appendChild(dot);
+});
